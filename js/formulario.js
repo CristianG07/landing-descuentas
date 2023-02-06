117a92362186f325bc7581a17f03590290974b51
CristianG07 <crisgonzalez07g@gmail.com>
2023-02-06 14:34:32 -0500

    updated responsive

---
+++ js/formulario.js
@@ -0,0 +1,11 @@
+// const scriptURL = 'https://sheet.best/api/sheets/58786077-4224-4479-9429-4f6eeb413358';
+const form = document.forms['FORMULARIO']
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  location.href = '../index.html';
+  // fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  //     .then(response => console.log('Success!', response))
+  //     .then(() => location.href = '../index.html')
+  //     .catch(error => console.error('Error!', error.message))
+});
