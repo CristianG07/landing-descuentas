84019d3f994f56fbb43d06105a63bb5ab9007923
CristianG07 <crisgonzalez07g@gmail.com>
2023-02-10 11:30:48 -0500

    descuentas

--- pages/connection.php
+++ pages/connection.php
@@ -1,12 +1,19 @@
 <?php
 
 //validamos datos del servidor
-$DB_USER = "descuentas_smoke";
-$DB_PASSWORD = "n27e-+}+G=NbBK#lVFT6gR1qsOWQ+aZs";
-$DB_HOST = "http://dev.descuentas.com/db-connect-test.php";
-$DB_NAME = "descuentas_smoketest";
+$DB_USER = "root";
+$DB_PASSWORD = "6rMMGRLwVBaiATvH9I75";
+$DB_HOST = "containers-us-west-182.railway.app";
+$DB_NAME = "railway";
+$DB_PORT = "6581";
 
 //conetamos al base datos
-$connec = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+$connec = mysqli_connect("mysql://",$DB_USER,":",$DB_PASSWORD,"@",$DB_HOST,":",$DB_PORT,"/",$DB_NAME);
+
+if ($connec) {
+  echo "CONECTADO";
+} else {
+  echo "NO CONECTADO";
+}
 
 ?>
