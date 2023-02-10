0de9cd7a6ee35db040d35349a4a863ad12d8bc45
CristianG07 <crisgonzalez07g@gmail.com>
2023-02-10 09:30:00 -0500

    descuentas

--- pages/connection.php
+++ pages/connection.php
@@ -3,7 +3,7 @@
 //validamos datos del servidor
 $DB_USER = "descuentas_smoke";
 $DB_PASSWORD = "n27e-+}+G=NbBK#lVFT6gR1qsOWQ+aZs";
-$DB_HOST = "45.32.140.42";
+$DB_HOST = "http://dev.descuentas.com/db-connect-test.php";
 $DB_NAME = "descuentas_smoketest";
 
 //conetamos al base datos
