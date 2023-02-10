2519a3fd9c39eeee0810042fdf95184acb2fc51d
CristianG07 <crisgonzalez07g@gmail.com>
2023-02-09 23:19:36 -0500

    descuentas completed

--- pages/connection.php
+++ pages/connection.php
@@ -1,12 +1,15 @@
 <?php
 
 //validamos datos del servidor
-$user = "root";
-$pass = "";
-$host = "localhost";
-$dbname = "registros";
+$DB_USER = "root";
+$DB_PASSWORD = "";
+$DB_HOST = "localhost";
+$DB_NAME = "registros";
+// $DB_URL = "registros";
+// $DB_PORT = "registros";
 
 //conetamos al base datos
-$connec = mysqli_connect($host, $user, $pass, $dbname);
+// $connec = mysqli_connect('$DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME');
+$connec = mysqli_connect('mysql://root:6rMMGRLwVBaiATvH9I75@containers-us-west-182.railway.app:6581/railway');
 
 ?>
