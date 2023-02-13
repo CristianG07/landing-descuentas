22ecb59a4c6c34b91f2651f586baead3ec5d5f64
CristianG07 <crisgonzalez07g@gmail.com>
2023-02-12 22:39:04 -0500

    descuentas

--- connection.php
+++ connection.php
@@ -8,7 +8,7 @@ $DB_NAME = "railway";
 $DB_PORT = 6581;
 
 //conetamos al base datos
-$connec = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+$connec = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
 // $connec = mysqli_connect("mysql://root:6rMMGRLwVBaiATvH9I75@containers-us-west-182.railway.app:6581/railway");
 
 if (!$connec) {
