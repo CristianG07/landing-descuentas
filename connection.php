b2ea07f9fc6e4b7e11b95351498cd6bc7ad82aa0
CristianG07 <crisgonzalez07g@gmail.com>
2023-02-12 23:22:13 -0500

    conecction railway

--- connection.php
+++ connection.php
@@ -2,10 +2,10 @@
 
 //validamos datos del servidor
 $DB_USER = "root";
-$DB_HOST = "containers-us-west-182.railway.app";
-$DB_PASSWORD = "6rMMGRLwVBaiATvH9I75";
+$DB_HOST = "containers-us-west-136.railway.app";
+$DB_PASSWORD = "pchI5VEgjuDmbWmx9dKZ";
 $DB_NAME = "railway";
-$DB_PORT = 6581;
+$DB_PORT = 6429;
 
 //conetamos al base datos
 $connec = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
