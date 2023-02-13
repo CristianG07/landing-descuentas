56a81a7212a8ab8283156d31ab7a99660175dfc9
CristianG07 <crisgonzalez07g@gmail.com>
2023-02-12 22:42:14 -0500

    conecction raylwail

--- connection.php
+++ connection.php
@@ -9,12 +9,11 @@ $DB_PORT = 6581;
 
 //conetamos al base datos
 $connec = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
-// $connec = mysqli_connect("mysql://root:6rMMGRLwVBaiATvH9I75@containers-us-west-182.railway.app:6581/railway");
 
-if (!$connec) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-mysqli_close($connec);
+// if (!$connec) {
+//   die("Connection failed: " . mysqli_connect_error());
+// }
+// echo "Connected successfully";
+// mysqli_close($connec);
 
 ?>
