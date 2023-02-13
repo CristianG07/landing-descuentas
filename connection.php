50ae2ac442abec3af5cd29b2f8c1a26a019c317d
CristianG07 <crisgonzalez07g@gmail.com>
2023-02-12 22:36:04 -0500

    descuentas

--- connection.php
+++ connection.php
@@ -1,15 +1,15 @@
 <?php
 
 //validamos datos del servidor
-$DB_USER = "descuentas_smoke";
-$DB_HOST = "localhost";
-$DB_PASSWORD = "n27e-+}+G=NbBK#lVFT6gR1qsOWQ+aZs";
-$DB_NAME = "descuentas_smoketest";
-// $DB_PORT = "6581";
+$DB_USER = "root";
+$DB_HOST = "containers-us-west-182.railway.app";
+$DB_PASSWORD = "6rMMGRLwVBaiATvH9I75";
+$DB_NAME = "railway";
+$DB_PORT = 6581;
 
 //conetamos al base datos
-// $connec = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
-$connec = mysqli_connect("mysql://root:6rMMGRLwVBaiATvH9I75@containers-us-west-182.railway.app:6581/railway");
+$connec = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+// $connec = mysqli_connect("mysql://root:6rMMGRLwVBaiATvH9I75@containers-us-west-182.railway.app:6581/railway");
 
 if (!$connec) {
   die("Connection failed: " . mysqli_connect_error());
